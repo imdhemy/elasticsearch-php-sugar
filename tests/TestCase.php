b5076b5fc3694ea859c837e582b4544bd340931d
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-01 09:50:15 +0200

    chore(tests): add faker instance

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Imdhemy\EsSugar\Tests;
+
+use Imdhemy\EsUtils\Faker;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var Faker
+     */
+    protected Faker $faker;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Faker::create();
+    }
+}
